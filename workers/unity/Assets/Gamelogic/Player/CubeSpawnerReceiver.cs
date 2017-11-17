8a8087974d5461f3b77c9ef86da94c6729f1aa9b
Antonio Marrazzo <a.marrazzo19@studenti.unisa.it>
2017-11-17 12:03:09 +0100

    Initial commit

---
+++ workers/unity/Assets/Gamelogic/Player/CubeSpawnerReceiver.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Gamelogic.EntityTemplates;
+using Improbable;
+using Improbable.Player;
+using Improbable.Unity;
+using Improbable.Unity.Core;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
+
+namespace Assets.Gamelogic.Player
+{
+	[WorkerType(WorkerPlatform.UnityWorker)]
+	public class CubeSpawnerReceiver : MonoBehaviour
+	{
+		[Require] private Position.Writer PositionWriter;
+		[Require] private CubeSpawner.Reader CubeSpawnerReader;
+
+		private void OnEnable()
+		{
+			CubeSpawnerReader.SpawnTriggered.Add(CreateCube);
+		}
+
+		private void OnDisable()
+		{
+			CubeSpawnerReader.SpawnTriggered.Remove(CreateCube);
+		}
+
+		private void CreateCube(Spawn args)
+		{
+			var cubeTemplate = EntityTemplateFactory.CreateCubeTemplate(args.initialPosition);
+			SpatialOS.Commands.CreateEntity(PositionWriter, cubeTemplate);
+		}
+	}
+}
---
+++ workers/unity/Assets/Gamelogic/Player/CubeSpawnerSender.cs
@@ -0,0 +1,24 @@
+﻿using Improbable.Player;
+using Improbable.Unity;
+using Improbable.Unity.Common.Core.Math;
+using Improbable.Unity.Visualizer;
+using UnityEngine;
+
+namespace Assets.Gamelogic.Player
+{
+	[WorkerType(WorkerPlatform.UnityClient)]
+	public class CubeSpawnerSender : MonoBehaviour
+	{
+		[Require] private CubeSpawner.Writer CubeSpawnerWriter;
+
+		private void Update()
+		{
+			if (Input.GetMouseButtonUp(0)) {
+				var spawnPosition = new Vector3(0, 3, 0);
+
+				CubeSpawnerWriter.Send(new CubeSpawner.Update()
+					.AddSpawn(new Spawn(spawnPosition.ToSpatialCoordinates())));
+			}
+		}
+	}
+}
